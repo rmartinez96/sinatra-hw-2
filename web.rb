3188cbbec6c4fa6c714f5046aaa5577e3d13a0b6
Eric Martinez <eric.michael.mtz@gmail.com>
2017-10-12 12:20:23 -0500

    fix for heroku deployment

--- web.rb
+++ web.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 require 'data_mapper' # metagem, requires common plugins too.
 
 # need install dm-sqlite-adapter
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+# if on heroku, use Postgres database
+# if not use sqlite3 database I gave you
+if ENV['DATABASE_URL']
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+else
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+end
 
 class Post
     include DataMapper::Resource
