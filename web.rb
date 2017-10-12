18c20911a87ffbd6e38ae94ad1e249d325f0f65b
Eric Martinez <eric.michael.mtz@gmail.com>
2017-10-12 13:39:23 -0500

    Minor change

--- web.rb
+++ web.rb
@@ -5,7 +5,7 @@ require 'data_mapper' # metagem, requires common plugins too.
 # if on heroku, use Postgres database
 # if not use sqlite3 database I gave you
 if ENV['DATABASE_URL']
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 else
   DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
 end
